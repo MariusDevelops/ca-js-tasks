d21702399a169cd80bb353907e2b28a9eb863465
MariusDevelops <brazasmarius@gmail.com>
2022-12-04 21:54:19 +0200

    feat: solved 7-8 tasks in classes-tasks-2.js

--- 3rd-week/classes-tasks-2.js
+++ 3rd-week/classes-tasks-2.js
@@ -237,13 +237,194 @@ console.groupEnd();
 
 console.groupCollapsed("7. - https://edabit.com/challenge/HKmJFmZZCX53ff4ke");
 {
-  // ... code
+  function sweetestIcecream(arr) {
+    const flavors = {
+      Plain: 0,
+      Vanilla: 5,
+      ChocolateChip: 5,
+      Strawberry: 10,
+      Chocolate: 10,
+    };
+
+    return Math.max(
+      ...arr.map((ice) => flavors[ice.flavor] + ice.numSprinkles)
+    );
+  }
+
+  class IceCream {
+    constructor(flavor, numSprinkles) {
+      this.flavor = flavor;
+      this.numSprinkles = numSprinkles;
+    }
+  }
+
+  const ice1 = new IceCream("Chocolate", 13);
+  const ice2 = new IceCream("Vanilla", 0);
+  const ice3 = new IceCream("Strawberry", 7);
+  const ice4 = new IceCream("Plain", 18);
+  const ice5 = new IceCream("ChocolateChip", 3);
+  const ice6 = new IceCream("Chocolate", 23);
+  const ice7 = new IceCream("Strawberry", 0);
+  const ice8 = new IceCream("Plain", 34);
+  const ice9 = new IceCream("Plain", 81);
+  const ice10 = new IceCream("Vanilla", 12);
+
+  console.log(sweetestIcecream([ice1, ice2, ice3, ice4, ice5]), 23);
+  console.log(
+    sweetestIcecream([ice7, ice10, ice1, ice6, ice8, ice10, ice2, ice2]),
+    34
+  );
+  console.log(sweetestIcecream([ice10, ice10, ice6, ice8, ice4]), 34);
+  console.log(sweetestIcecream([ice2, ice10, ice6, ice9, ice7]), 81);
+  console.log(
+    sweetestIcecream([ice10, ice6, ice4, ice1, ice7, ice8, ice6]),
+    34
+  );
+  console.log(sweetestIcecream([ice3, ice1]), 23);
+  console.log(sweetestIcecream([ice6, ice7, ice5, ice4, ice3]), 33);
+  console.log(sweetestIcecream([ice4, ice8, ice9]), 81);
+  console.log(sweetestIcecream([ice5, ice7]), 10);
+  console.log(
+    sweetestIcecream([ice5, ice3, ice6, ice2, ice7, ice2, ice7, ice2]),
+    33
+  );
+  console.log(
+    sweetestIcecream([ice1, ice9, ice10, ice9, ice7, ice1, ice9]),
+    81
+  );
+  console.log(sweetestIcecream([ice1, ice4]), 23);
+  console.log(sweetestIcecream([ice7, ice4]), 18);
+  console.log(
+    sweetestIcecream([ice1, ice8, ice6, ice7, ice3, ice2, ice3, ice7]),
+    34
+  );
+  console.log(sweetestIcecream([ice7, ice8, ice4, ice4, ice5, ice1]), 34);
+  console.log(sweetestIcecream([ice10, ice10, ice9, ice4, ice7, ice9]), 81);
+  console.log(sweetestIcecream([ice3, ice10, ice1]), 23);
+  console.log(
+    sweetestIcecream([ice3, ice4, ice7, ice3, ice7, ice10, ice2]),
+    18
+  );
+  console.log(
+    sweetestIcecream([ice5, ice9, ice9, ice9, ice7, ice5, ice9, ice7]),
+    81
+  );
+  console.log(sweetestIcecream([ice4, ice9, ice2]), 81);
+  console.log(sweetestIcecream([ice8, ice2, ice2, ice2, ice4, ice8]), 34);
+  console.log(
+    sweetestIcecream([ice4, ice9, ice4, ice3, ice3, ice2, ice5, ice2]),
+    81
+  );
+  console.log(sweetestIcecream([ice8, ice10, ice5]), 34);
+  console.log(sweetestIcecream([ice10, ice3, ice2, ice1, ice9]), 81);
+  console.log(sweetestIcecream([ice8, ice3, ice4, ice5]), 34);
+  console.log(sweetestIcecream([ice10, ice8, ice6, ice7, ice9, ice4]), 81);
+  console.log(sweetestIcecream([ice5, ice4, ice6, ice6, ice1]), 33);
+  console.log(sweetestIcecream([ice6, ice8, ice2, ice10, ice7, ice10]), 34);
+  console.log(sweetestIcecream([ice1, ice9, ice7, ice3]), 81);
+  console.log(
+    sweetestIcecream([ice7, ice1, ice9, ice6, ice7, ice10, ice2, ice3]),
+    81
+  );
+  console.log(sweetestIcecream([ice5, ice1, ice7, ice6, ice8, ice1, ice8]), 34);
+  console.log(sweetestIcecream([ice10, ice9, ice2, ice4, ice10]), 81);
+  console.log(sweetestIcecream([ice3, ice7, ice10]), 17);
+  console.log(sweetestIcecream([ice10, ice5, ice4]), 18);
+  console.log(sweetestIcecream([ice9, ice2, ice4, ice8, ice3, ice3]), 81);
+  console.log(sweetestIcecream([ice6, ice3, ice9, ice8, ice2, ice6]), 81);
+  console.log(sweetestIcecream([ice10, ice3]), 17);
+  console.log(sweetestIcecream([ice10, ice7, ice5, ice2, ice9]), 81);
+  console.log(sweetestIcecream([ice10, ice10, ice4, ice1, ice9]), 81);
+  console.log(sweetestIcecream([ice9, ice2, ice6, ice4, ice10, ice3]), 81);
+  console.log(
+    sweetestIcecream([ice8, ice10, ice1, ice7, ice8, ice9, ice1]),
+    81
+  );
+  console.log(sweetestIcecream([ice7, ice5, ice3, ice8, ice1, ice9]), 81);
+  console.log(sweetestIcecream([ice2, ice6, ice3]), 33);
+  console.log(sweetestIcecream([ice6, ice6]), 33);
+  console.log(sweetestIcecream([ice9, ice6, ice8, ice3, ice2, ice2]), 81);
+  console.log(sweetestIcecream([ice1, ice3, ice3, ice6]), 33);
+  console.log(sweetestIcecream([ice8, ice6]), 34);
+  console.log(sweetestIcecream([ice1, ice1]), 23);
+  console.log(
+    sweetestIcecream([ice4, ice2, ice3, ice9, ice5, ice10, ice6]),
+    81
+  );
+  console.log(
+    sweetestIcecream([ice10, ice8, ice4, ice3, ice9, ice8, ice1, ice10]),
+    81
+  );
+  console.log(sweetestIcecream([ice5, ice8, ice5]), 34);
 }
 console.groupEnd();
 
 console.groupCollapsed("8. - https://edabit.com/challenge/9zwdrfW99zmdRhibi");
 {
-  // ... code
+  class OnesThreesNines {
+    constructor(number) {
+      this.ones = Math.floor(number / 1);
+      this.threes = Math.floor(number / 3);
+      this.nines = Math.floor(number / 9);
+    }
+  }
+
+  const probs = [
+    {
+      num: 0,
+      which: "ones",
+      expect: 0,
+    },
+    {
+      num: 1,
+      which: "threes",
+      expect: 0,
+    },
+    {
+      num: 2,
+      which: "nines",
+      expect: 0,
+    },
+    {
+      num: 3,
+      which: "ones",
+      expect: 3,
+    },
+    {
+      num: 4,
+      which: "threes",
+      expect: 1,
+    },
+    {
+      num: 10,
+      which: "nines",
+      expect: 1,
+    },
+    {
+      num: 13,
+      which: "ones",
+      expect: 13,
+    },
+    {
+      num: 15,
+      which: "threes",
+      expect: 5,
+    },
+    {
+      num: 17,
+      which: "nines",
+      expect: 1,
+    },
+    {
+      num: 20,
+      which: "nines",
+      expect: 2,
+    },
+  ];
+  probs.forEach((prob) => {
+    let testInstance = new OnesThreesNines(prob.num);
+    console.log(testInstance[prob.which], prob.expect);
+  });
 }
 console.groupEnd();
 
